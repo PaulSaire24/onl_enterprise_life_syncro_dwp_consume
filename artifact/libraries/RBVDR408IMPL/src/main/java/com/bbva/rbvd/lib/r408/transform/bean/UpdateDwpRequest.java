92d3d1c9322d98679ffa655e10a760abe5364a8a
P041360 <marco.yovera@bbva.com>
2024-05-31 18:09:23 -0500

    feat: PARSRBVD-3225 PARSRBVD-PARSRBVD-3253 modification in channel set

--- artifact/libraries/RBVDR408IMPL/src/main/java/com/bbva/rbvd/lib/r408/transform/bean/UpdateDwpRequest.java
+++ artifact/libraries/RBVDR408IMPL/src/main/java/com/bbva/rbvd/lib/r408/transform/bean/UpdateDwpRequest.java
@@ -8,6 +8,7 @@ import com.bbva.rbvd.dto.payroll.utils.ConstantsUtils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class UpdateDwpRequest {
     private UpdateDwpRequest(){}
@@ -26,7 +27,7 @@ public class UpdateDwpRequest {
 
         salesForceBO.setAuditUser(requestBody.getAuditUser());
         DescriptionDTO channel = new DescriptionDTO();
-        channel.setId(requestBody.getChannel().getId()=="PC"?ConstantsUtils.DwpQuotationPayrollStatus.CHANNEL_FU: "SENDA") ;
+        channel.setId(Objects.equals(requestBody.getChannel().getId(), "PC") ?ConstantsUtils.DwpQuotationPayrollStatus.CHANNEL_FU: "SENDA") ;
         salesForceBO.setChannel(requestBody.getChannel());
 
         LocalDate currentDate = LocalDate.now();
